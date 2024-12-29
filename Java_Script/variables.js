3a5c382be1ee2ade10b82310088c65b392135c83
Rukham Khalid <rukhamkhalid08@outlook.com>
2024-12-29 12:03:17 +0500

    Task

---
+++ Java_Script/variables.js
@@ -0,0 +1,20 @@
+let a=78;
+console.log(a)
+a="Rukham";
+console.log(a)
+console.log("Javascript:var,let and const")
+//var in java old usage-rediclare
+var b=45;
+var c="Ruhiii";
+var d=null
+var b="JAVA";
+console.log(b)
+// let in java let is blockscoped-usualy used
+let e=67;
+console.log(e)
+e="Ruhiii";
+console.log(e)
+//const in java 
+const author="Rukham";
+//let author=5 //this line error occur bcz constant cannot be changed
+console.log(author)
